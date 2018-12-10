508c8d0f143babe2ac4fbdb86aa64eb648c3e848
azhigaylo_home <antoooon@gmail.com>
2018-12-10 22:18:50 +0200

    rename build file

--- env.sh
+++ env.sh
@@ -3,7 +3,6 @@
 # set Qt5 *.pc files 
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/home/azhigaylo/Qt/5.11.1/gcc_64/lib/pkgconfig/
 
-
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/azhigaylo/project/homebrain_third_party/mosquitto/lib
 export CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH:/home/azhigaylo/project/homebrain_third_party/mosquitto/
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/home/azhigaylo/project/homebrain_third_party/mosquitto/lib/pkgconfig/
