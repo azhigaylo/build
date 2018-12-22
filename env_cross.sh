c759d16cba56e8efc2aad364e08e233504052a94
azhigaylo <anton.zhigaylo@apostera.com>
2018-12-22 18:50:08 +0200

    added auto source

--- env_cross.sh
+++ env_cross.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/azhigaylo/project/homebrain_third_party/cross/dlt-daemon/lib/
-export CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH:/home/azhigaylo/project/homebrain_third_party/cross/dlt-daemon/
-export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/home/azhigaylo/project/homebrain_third_party/cross/dlt-daemon/lib/pkgconfig/
+readonly local _project_root_path=$( dirname $( dirname $( dirname $( realpath -s $0))))
+readonly local _project_name=$( basename $( dirname $( dirname $( realpath -s $0} ) ) ))
+readonly local _source_files_path="$_project_root_path"/"$_project_name"_third_party/cross
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/azhigaylo/project/homebrain_third_party/cross/mosquitto/lib
-export CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH:/home/azhigaylo/project/homebrain_third_party/cross/mosquitto/
-export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/home/azhigaylo/project/homebrain_third_party/cross/mosquitto/lib/pkgconfig/
-
-echo " ---> PKG_CONFIG_PATH = $PKG_CONFIG_PATH "
-echo " ---> CMAKE_PREFIX_PATH = $CMAKE_PREFIX_PATH "
+cd "$_source_files_path"
+    for _env_file in **/set_*_env.sh; do
+        echo " ---> source $_env_file"
+        source $_env_file > /dev/null;
+    done
+    echo " ---> source result:"
+    echo " ---> LD_LIBRARY_PATH='$LD_LIBRARY_PATH'"
+    echo " ---> CMAKE_PREFIX_PATH='$CMAKE_PREFIX_PATH'"
+    echo " ---> PKG_CONFIG_PATH='$PKG_CONFIG_PATH'"
+cd -
--- env_host.sh
+++ env_host.sh
@@ -3,13 +3,17 @@
 # set Qt5 *.pc files 
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/home/azhigaylo/Qt/5.11.1/gcc_64/lib/pkgconfig/
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/azhigaylo/project/homebrain_third_party/host/dlt-daemon/lib/
-export CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH:/home/azhigaylo/project/homebrain_third_party/host/dlt-daemon/
-export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/home/azhigaylo/project/homebrain_third_party/host/dlt-daemon/lib/pkgconfig/
+readonly local _project_root_path=$( dirname $( dirname $( dirname $( realpath -s $0))))
+readonly local _project_name=$( basename $( dirname $( dirname $( realpath -s $0} ) ) ))
+readonly local _source_files_path="$_project_root_path"/"$_project_name"_third_party/host
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/azhigaylo/project/homebrain_third_party/host/mosquitto/lib
-export CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH:/home/azhigaylo/project/homebrain_third_party/host/mosquitto/
-export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/home/azhigaylo/project/homebrain_third_party/host/mosquitto/lib/pkgconfig/
-
-echo " ---> PKG_CONFIG_PATH = $PKG_CONFIG_PATH "
-echo " ---> CMAKE_PREFIX_PATH = $CMAKE_PREFIX_PATH "
+cd "$_source_files_path"
+    for _env_file in **/set_*_env.sh; do
+        echo " ---> source $_env_file"
+        source $_env_file > /dev/null;
+    done
+    echo " ---> source result:"
+    echo " ---> LD_LIBRARY_PATH='$LD_LIBRARY_PATH'"
+    echo " ---> CMAKE_PREFIX_PATH='$CMAKE_PREFIX_PATH'"
+    echo " ---> PKG_CONFIG_PATH='$PKG_CONFIG_PATH'"
+cd -
